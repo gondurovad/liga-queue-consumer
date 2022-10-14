ed19fbe9c06dcd97c4f382aea8b93faaa648b927
Гондурова Дарья <daryavgrei@gmail.com>
2022-10-14 16:24:42 +0300

    add rabbit configuration

--- src/main/java/liga/medical/queueconsumer/ConsumerApplication.java
+++ src/main/java/liga/medical/queueconsumer/ConsumerApplication.java
@@ -1,9 +1,12 @@
 package liga.medical.queueconsumer;
 
+import liga.medical.queueconsumer.config.RabbitConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(RabbitConfig.class)
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
